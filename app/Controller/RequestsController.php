d43f298b87ef11a5ef055c613a71012a669b3bcf
David Barbarisi <davidbarbarisi@gmail.com>
2011-11-05 01:33:48 -0400

    added some empty actions to the controllers

--- app/Controller/RequestsController.php
+++ app/Controller/RequestsController.php
@@ -5,6 +5,18 @@ class RequestsController extends AppController {
     public $name = 'Requests';
 
     function index() {
+        // if it's a POST, delete selected requests, or clear all
         $this->set('requests', $this->Request->find('all'));
+    }
+    
+    function add($uri) {
+      // if  it's a valid spotify uri, add/update the request model
+      // tellt eh user the track was added
+      // redirect to the home page
+    }
+    
+    function delete($uri) {
+      // tell the user the request was removed
+      // redirect to the home page
     }
 }
--- app/Controller/SpotifyTracksController.php
+++ app/Controller/SpotifyTracksController.php
@@ -7,4 +7,19 @@ class SpotifyTracksController extends AppController {
     function index() {
         $this->set('spotify_tracks', $this->SpotifyTrack->find('all'));
     }
+    
+    function search($search = NULL) {
+      // show a search box
+      if ($search) {
+        // do a spotify api call and set the results to the view
+        // save the results to the spotifytrack model
+      }
+    }
+    
+    function autocomplete($str = NULL) {
+      if ($str) {
+        // query the spotifytrack model
+        // set the results for a view that returns json
+      }
+    }
 }
